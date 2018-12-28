15d68a0eed9d81062034ad1bfd1617feb2e7a0f7
Kane Dou <douqilong@gmail.com>
2018-12-28 17:01:16 +0800

    make http transport aware of proxy from envvar

--- tunneldns/https_upstream.go
+++ tunneldns/https_upstream.go
@@ -39,6 +39,7 @@ func NewUpstreamHTTPS(endpoint string) (Upstream, error) {
 		TLSClientConfig:    tls,
 		DisableCompression: true,
 		MaxIdleConns:       1,
+		Proxy:              http.ProxyFromEnvironment,
 	}
 	http2.ConfigureTransport(transport)
 
