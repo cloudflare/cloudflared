78203515fd8282d3b091e29eeb57d9705fa38380
Johan Westling <johan.westling@paf.com>
2020-07-23 08:20:25 +0300

    Added WSL2 check to unix browser launcher

--- cmd/cloudflared/shell/launch_browser_unix.go
+++ cmd/cloudflared/shell/launch_browser_unix.go
@@ -3,9 +3,15 @@
 package shell
 
 import (
+	"os"
 	"os/exec"
 )
 
 func getBrowserCmd(url string) *exec.Cmd {
+	// Check for Windows Subsystem for Linux (v2+).
+	if os.Getenv("WSL_DISTRO_NAME") != "" {
+		return exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", url)
+	}
+
 	return exec.Command("xdg-open", url)
 }
