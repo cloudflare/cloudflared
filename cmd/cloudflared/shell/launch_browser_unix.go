6c81dc291e2a565f68a9c81d2b296e0e8a16ead6
Johan Westling <johan.westling@paf.com>
2020-07-23 10:06:40 +0300

    Minor update to comment

--- cmd/cloudflared/shell/launch_browser_unix.go
+++ cmd/cloudflared/shell/launch_browser_unix.go
@@ -8,7 +8,7 @@ import (
 )
 
 func getBrowserCmd(url string) *exec.Cmd {
-	// Check for Windows Subsystem for Linux (v2+).
+	// Check if wslview is available (Windows Subsystem for Linux).
 	if _, err := os.Stat("/usr/bin/wslview"); err == nil {
 		return exec.Command("wslview", url)
 	}
