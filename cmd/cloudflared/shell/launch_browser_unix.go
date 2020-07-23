7f83927fd2baa602b413d99bcebfae40f301e1ed
Johan Westling <johan.westling@paf.com>
2020-07-23 10:05:11 +0300

    Check if wslview exists and use it over xdg-open for WSL2

--- cmd/cloudflared/shell/launch_browser_unix.go
+++ cmd/cloudflared/shell/launch_browser_unix.go
@@ -9,8 +9,8 @@ import (
 
 func getBrowserCmd(url string) *exec.Cmd {
 	// Check for Windows Subsystem for Linux (v2+).
-	if os.Getenv("WSL_DISTRO_NAME") != "" {
-		return exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", url)
+	if _, err := os.Stat("/usr/bin/wslview"); err == nil {
+		return exec.Command("wslview", url)
 	}
 
 	return exec.Command("xdg-open", url)
