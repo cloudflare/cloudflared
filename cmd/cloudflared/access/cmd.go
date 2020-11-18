a4f185fd28fc9cc22a72b1c7b2da913d35930ba9
Security Generation <1046239+securitygeneration@users.noreply.github.com>
2020-11-18 15:15:54 +0100

    Update error message to use login command
    Unless I'm mistaken, when there is no existing token for an app, the `login` command needs to be run to obtain a token (not the `token` command, which itself doesn't generate a token).

--- cmd/cloudflared/access/cmd.go
+++ cmd/cloudflared/access/cmd.go
@@ -291,7 +291,7 @@ func generateToken(c *cli.Context) error {
 	}
 	tok, err := token.GetAppTokenIfExists(appURL)
 	if err != nil || tok == "" {
-		fmt.Fprintln(os.Stderr, "Unable to find token for provided application. Please run token command to generate token.")
+		fmt.Fprintln(os.Stderr, "Unable to find token for provided application. Please run login command to generate token.")
 		return err
 	}
 
