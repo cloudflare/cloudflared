ff4cfeda0c6f743e15b17cf576b23782cbd85d5c
Nuno Diegues <nuno@cloudflare.com>
2022-02-10 12:59:15 +0000

    TUN-5184: Handle errors in bidrectional streaming (websocket#Stream) gracefully when 1 side has ended

--- ingress/origin_connection.go
+++ ingress/origin_connection.go
@@ -78,16 +78,3 @@ func (sp *socksProxyOverWSConnection) Stream(ctx context.Context, tunnelConn io.
 
 func (sp *socksProxyOverWSConnection) Close() {
 }
-
-// wsProxyConnection represents a bidirectional stream for a websocket connection to the origin
-type wsProxyConnection struct {
-	rwc io.ReadWriteCloser
-}
-
-func (conn *wsProxyConnection) Stream(ctx context.Context, tunnelConn io.ReadWriter, log *zerolog.Logger) {
-	websocket.Stream(tunnelConn, conn.rwc, log)
-}
-
-func (conn *wsProxyConnection) Close() {
-	conn.rwc.Close()
-}
