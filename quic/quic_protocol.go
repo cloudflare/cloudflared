20e36c5bf3e6d7a2de9e5fe80b9115815f9f8e9d
João Oliveirinha <joliveirinha@cloudflare.com>
2023-06-14 16:35:39 +0100

    TUN-7468: Increase the limit of incoming streams

--- quic/quic_protocol.go
+++ quic/quic_protocol.go
@@ -39,6 +39,9 @@ const (
 	HandshakeIdleTimeout = 5 * time.Second
 	MaxIdleTimeout       = 5 * time.Second
 	MaxIdlePingPeriod    = 1 * time.Second
+
+	// MaxIncomingStreams is 2^60, which is the maximum supported value by Quic-Go
+	MaxIncomingStreams = 1 << 60
 )
 
 // RequestServerStream is a stream to serve requests
