00cd7c333ce7e7493a3dff245aa79b0cbbd77090
chungthuang <chungting@cloudflare.com>
2023-12-20 05:27:34 +0000

    TUN-8072: Need to set GOCACHE in mac go installation script

--- .teamcity/mac/install-cloudflare-go.sh
+++ .teamcity/mac/install-cloudflare-go.sh
@@ -1,4 +1,8 @@
 cd /tmp/
+rm -rf go
+rm -rf gocache
+export GOCACHE=/tmp/gocache
+
 git clone -q https://github.com/cloudflare/go
 cd go/src
 # https://github.com/cloudflare/go/tree/34129e47042e214121b6bbff0ded4712debed18e is version go1.21.5-devel-cf
