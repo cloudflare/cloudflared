5e5f2f4d8ca897e12a755cc29ba216500da07a44
chungthuang <chungting@cloudflare.com>
2024-04-22 12:46:10 -0500

    TUN-8380: Add sleep before requesting quick tunnel as temporary fix for component tests

--- component-tests/test_quicktunnels.py
+++ component-tests/test_quicktunnels.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from conftest import CfdModes
 from constants import METRICS_PORT
+import time
 from util import LOGGER, start_cloudflared, wait_tunnel_ready, get_quicktunnel_url, send_requests
 
 class TestQuickTunnels:
@@ -9,6 +10,7 @@ class TestQuickTunnels:
         LOGGER.debug(config)
         with start_cloudflared(tmp_path, config, cfd_pre_args=["tunnel", "--ha-connections", "1"], cfd_args=["--hello-world"], new_process=True):
             wait_tunnel_ready(require_min_connections=1)
+            time.sleep(10)
             url = get_quicktunnel_url()
             send_requests(url, 3, True)
     
@@ -17,6 +19,7 @@ class TestQuickTunnels:
         LOGGER.debug(config)
         with start_cloudflared(tmp_path, config, cfd_pre_args=["tunnel", "--ha-connections", "1"], cfd_args=["--url", f"http://localhost:{METRICS_PORT}/"], new_process=True):
             wait_tunnel_ready(require_min_connections=1)
+            time.sleep(10)
             url = get_quicktunnel_url()
             send_requests(url+"/ready", 3, True)
 
